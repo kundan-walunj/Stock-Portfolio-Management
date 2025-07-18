3e24332463bc0f4917beda184c7f87f2bfd80694
KUNDAN WALUNJ <94464285+kundan-walunj@users.noreply.github.com>
2025-07-18 15:21:57 +0530

    Update WeatherForecastController.cs

--- Stock Portfolio Management/Controllers/WeatherForecastController.cs
+++ Stock Portfolio Management/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Stock_Portfolio_Management.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","dizzy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
